338a7ac69a4260b4f8733db68d492128b2a36fef
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2022-07-29 22:47:34 +0200

    Clean up and updates.

---
+++ heap-leak/src/main/java/org/sample/heap_leak/App.java
@@ -0,0 +1,12 @@
+package org.sample.heap_leak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
