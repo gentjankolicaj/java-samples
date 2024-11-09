62a98156942d4f95dea612c307e7427f2959d05f
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2024-11-09 16:14:36 +0100

    websocket: websocket test update.

--- websocket/src/test/java/org/sample/websocket/WebSocketConfigTest.java
+++ websocket/src/test/java/org/sample/websocket/WebSocketConfigTest.java
@@ -1,7 +1,21 @@
 package org.sample.websocket;
 
+import jakarta.websocket.ClientEndpoint;
+import jakarta.websocket.ContainerProvider;
+import jakarta.websocket.DeploymentException;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.WebSocketContainer;
+import jakarta.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Set;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.LifecycleException;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -12,14 +26,77 @@ import org.junit.jupiter.api.Test;
 class WebSocketConfigTest {
 
   @Test
-  void websocket() throws LifecycleException {
+  void websocket() throws LifecycleException, IOException, DeploymentException {
     TomcatServer tomcatServer = new TomcatServer(8080, TestWebSocketConfig.class);
     tomcatServer.start();
+
+    //websocket client request
+    WebSocketContainer wsContainer = ContainerProvider.getWebSocketContainer();
+    Session wsSession = wsContainer.connectToServer(TestClientEndpoint.class,
+        URI.create("ws://localhost:8080/test-endpoint"));
+
+    wsSession.getBasicRemote().sendText("Client test message.");
+
+    //stop tomcat after 11 seconds
+    Awaitility.await()
+        .timeout(Duration.ofSeconds(15))
+        .pollDelay(Duration.ofSeconds(14))
+        .untilAsserted(tomcatServer::stop);
 
     //blocking join until close is called.
     tomcatServer.join();
   }
 
+
+  @SuppressWarnings("unused")
+  @Slf4j
+  @ServerEndpoint("/test-endpoint")
+  @Getter
+  public static class TestServerEndpoint {
+
+    private Session session;
+
+    @OnOpen
+    public void onOpen(Session session) {
+      this.session = session;
+      log.info("server: session opened.");
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String message) throws IOException {
+      log.info("server received : {}", message);
+      session.getBasicRemote().sendText("return-back-" + message);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  @Slf4j
+  @ClientEndpoint
+  @Getter
+  public static class TestClientEndpoint {
+
+    private Session session;
+
+    @OnOpen
+    public void onOpen(Session session) {
+      this.session = session;
+      log.info("client: Session opened.");
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String message) {
+      log.info("client received : {}", message);
+    }
+  }
+
+
+  public static class TestWebSocketConfig extends WebSocketConfig {
+
+    public TestWebSocketConfig() {
+      super(Set.of(TestServerEndpoint.class), Set.of());
+    }
+
+  }
 
 
 }
