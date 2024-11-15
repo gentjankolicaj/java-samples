da8a50b998937ec3f8f06dea7cd7f5a080b1564f
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2024-11-15 21:44:52 +0100

    tomcat-embed-11: added read file sample.

---
+++ tomcat-embed-11/src/main/java/org/sample/tomcat_embed_11/servlet/file/ReadFileServlet.java
@@ -0,0 +1,87 @@
+package org.sample.tomcat_embed_11.servlet.file;
+
+
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.sample.tomcat_embed_11.servlet.TomcatServlet;
+
+/**
+ * @author gentjan kolicaj
+ * @Date: 11/15/24 9:13 PM
+ */
+@Slf4j
+public class ReadFileServlet extends TomcatServlet {
+
+  public static String getServerFile() {
+    return ReadFileServlet.class.getResource("/file/secret_file.txt").getPath();
+  }
+
+  @Override
+  public String getPattern() {
+    return "/download_file";
+  }
+
+  @Override
+  public String getName() {
+    return getClass().getSimpleName();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    printRequest(req);
+    ServletInputStream sis = req.getInputStream();
+    ServletOutputStream sos = resp.getOutputStream();
+
+    //read from request
+    inputTask(sis);
+
+    //write to response
+    downloadTask(sos);
+  }
+
+  private void printRequest(HttpServletRequest req) {
+    log.info("request uri: {}", req.getRequestURI());
+    log.info("request id: {}", req.getRequestId());
+    log.info("request url: {}", req.getRequestURL());
+    log.info("request servlet path: {}", req.getServletPath());
+    log.info("request headers: {}", req.getHeaderNames());
+  }
+
+  private void inputTask(ServletInputStream sis) {
+    try {
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      byte[] buffer = new byte[1024];
+      while ((sis.read(buffer)) != -1) {
+        baos.write(buffer);
+      }
+      log.info("read from request byte-array : {}", baos.toByteArray());
+    } catch (IOException ioe) {
+      log.debug("", ioe);
+    }
+  }
+
+  private void downloadTask(ServletOutputStream sos) {
+    try {
+      final String filePath = getServerFile();
+      FileInputStream fis = new FileInputStream(filePath);
+      byte[] fileBytes = fis.readAllBytes();
+      log.info("writing to response file: '{}' byte-array-length : {}", filePath, fileBytes.length);
+      sos.write(fileBytes);
+      //flush writer after finished writing.
+      sos.flush();
+      fis.close();
+    } catch (IOException ioe) {
+      {
+        log.error("", ioe);
+      }
+    }
+  }
+
+
+}
---
+++ tomcat-embed-11/src/test/java/org/sample/tomcat_embed_11/servlet/file/ReadFileServletTest.java
@@ -0,0 +1,87 @@
+package org.sample.tomcat_embed_11.servlet.file;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.jdev.jackson.ConfigurationException;
+import io.jdev.jackson.YamlConfigurations;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.LifecycleException;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+import org.sample.tomcat_embed_11.servlet.ConnectorProperties;
+import org.sample.tomcat_embed_11.servlet.TomcatServer;
+
+/**
+ * @author gentjan kolicaj
+ * @Date: 11/15/24 4:05 PM
+ */
+@SuppressWarnings("all")
+@Slf4j
+class ReadFileServletTest {
+
+  public static final String TOMCAT_SERVER_STOP_CALLED = "tomcatServer.stop() called.";
+
+  @Test
+  void test()
+      throws LifecycleException, IOException, InterruptedException, ConfigurationException {
+    ConnectorProperties props = YamlConfigurations.load(ConnectorProperties.class,
+        "/servlet/connector.yaml");
+    int port = 8003;
+    String path = "download_file";
+    props.setPort(port);
+
+    TomcatServer tomcatServer = new TomcatServer(props, List.of(new ReadFileServlet()));
+    tomcatServer.start();
+
+    // Create an HttpClient instance
+    HttpClient client = HttpClient.newHttpClient();
+
+    // Create an dummy http request
+    HttpRequest request1 = HttpRequest.newBuilder()
+        .uri(URI.create(String.format("http://localhost:%d", port)))
+        .GET()
+        .build();
+    HttpResponse<String> response1 = client.send(request1, HttpResponse.BodyHandlers.ofString());
+    assertThat(response1.statusCode()).isEqualTo(404);
+    assertThat(response1.body()).contains(
+        "The origin server did not find a current representation for the target resource or is not willing to disclose that one exists.");
+
+    // File download test
+    //=============================================================================================
+    //read file for asserting
+    byte[] fileBytes = null;
+    try (FileInputStream fis = new FileInputStream(ReadFileServlet.getServerFile())) {
+      fileBytes = fis.readAllBytes();
+    }
+    // Create an valid http request
+    HttpRequest request2 = HttpRequest.newBuilder()
+        .uri(URI.create(String.format("http://localhost:%d/%s", port, path)))
+        .GET()
+        .build();
+    HttpResponse<byte[]> response2 = client.send(request2, HttpResponse.BodyHandlers.ofByteArray());
+    assertThat(response2.statusCode()).isEqualTo(200);
+    assertThat(response2.body()).isNotEmpty();
+    assertThat(response2.body()).contains(fileBytes);
+
+    Awaitility.await()
+        .timeout(Duration.ofSeconds(25))
+        .pollDelay(Duration.ofSeconds(24))
+        .untilAsserted(() -> {
+          tomcatServer.stop();
+          log.warn(TOMCAT_SERVER_STOP_CALLED);
+        });
+
+    //blocking join until close is called.
+    tomcatServer.join();
+  }
+
+
+}
