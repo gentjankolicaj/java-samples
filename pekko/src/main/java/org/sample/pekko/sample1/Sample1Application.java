033c14ef4d46d79d7893498787de24ebd3c8de26
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2026-01-21 20:59:43 +0100

    pekko: sample 1 finished.

---
+++ pekko/src/main/java/org/sample/pekko/sample1/Sample1Application.java
@@ -0,0 +1,18 @@
+package org.sample.pekko.sample1;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ * @author gentjan kolicaj
+ * @since 1/21/26 5:39 PM
+ *
+ */
+@Slf4j
+public class Sample1Application {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ pekko/src/main/java/org/sample/pekko/sample1/readme.md
@@ -0,0 +1,1 @@
+# pekko sample 1
